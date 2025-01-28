407f76bfda8652de19cb92f8724cf31c3bccf861
Zach Sparrow <zmn6@cornell.edu>
2025-01-28 10:32:47 -0500

    several robustness checks and unit tests

--- src/aabbcd/aabb.py
+++ src/aabbcd/aabb.py
@@ -23,7 +23,7 @@ def draw_aabb(dist: np.ndarray, eps: float, verbose=False):
         else:
             return [[]]
     if not isinstance(verbose, bool):
-        raise ValueError("Verbose option must be a boolean")
+        raise TypeError("Verbose option must be a boolean.")
 
     dim = dist.ndim
     marginal_eps = eps / dim
@@ -47,12 +47,29 @@ def draw_aabb(dist: np.ndarray, eps: float, verbose=False):
 
 
 def marginalize(dist, axis):
+    if not isinstance(dist, np.ndarray):
+        raise TypeError("dist must be a numpy array.")
+    if not isinstance(axis, int):
+        raise TypeError("axis must be an integer.")
+    if axis > dist.ndim-1:
+        raise ValueError("Marginalization axis greater than number of array dimensions.")
     axes = tuple(x for x in range(dist.ndim) if x != axis)
     marginal = np.sum(dist, axis = axes)
     return marginal
 
 
 def marginal_bound(dist, eps):
+    if not isinstance(dist, np.ndarray):
+        raise TypeError("dist must be a numpy array.")
+    if not isinstance(eps, float):
+        raise TypeError("eps must be a float.")
+    if eps < 0.0 or eps > 1.0:
+        raise ValueError("eps must be between 0 and 1.")
+    if eps == 1.0:
+        return []
+    if np.any(dist < 0):
+        raise ValueError("dist contains negative numbers.")
+    dist_norm = np.sum(dist)
     pos_list = list(range(len(dist)))
     center = sum([d*pos for d, pos in zip(dist, pos_list)])
     cost_list = [len(dist) - np.abs(c - center) for c in pos_list]
@@ -63,7 +80,7 @@ def marginal_bound(dist, eps):
     lb = max(ordering)
     rb = min(ordering)
     i = 0
-    while norm < 1.0 - eps:
+    while norm < (1.0 - eps)*dist_norm:
         norm += dist[ordering[i]]
         lb = min(ordering[i], lb)
         rb = max(ordering[i], rb)
@@ -72,6 +89,24 @@ def marginal_bound(dist, eps):
 
 
 def trunc_dist(dist, bounds):
+    if not isinstance(dist, np.ndarray):
+        raise TypeError("dist must be a numpy array.")
+    if np.any(dist < 0):
+        raise ValueError("dist contains negative numbers.")
+    if len(bounds) != dist.ndim:
+        raise ValueError("bounds and dist don't have same dimensionality.")
+    dist_dims = dist.shape
+    if not isinstance(bounds, list):
+        raise TypeError("Bounds must be a list.")
+    for b,s in zip(bounds, dist_dims):
+        if not isinstance(b, list):
+            raise TypeError("Bounds along at least one axis are not a list.")
+        if len(b) != 2:
+            raise ValueError("Too many bounds along at least one axis.")
+        if b[0] > b[1]:
+            raise ValueError("Bounds are out of order (right bound is further left than left bound).")
+        if b[1] > s:
+            raise ValueError("Bounds along at least one dimension are longer than size of array")
     index = tuple(slice(b[0],b[1],1) for b in bounds)
     truncated_dist = dist[index]
     return truncated_dist
--- tests/tests.py
+++ tests/tests.py
@@ -1,37 +1,100 @@
 import sys
 sys.path.append("../src/aabbcd")
 
-from aabb import draw_aabb
+import aabb as aabb
 import numpy as np
 import math
 from scipy.stats import multivariate_normal
 
+print("Unit testing...")
+test_dist = np.array([[1,1],[2,2]])
+marginal_test_1 = aabb.marginalize(test_dist, 0)
+marginal_test_2 = aabb.marginalize(test_dist, 1)
+assert((marginal_test_1 == np.array([2,4])).all()), "Marginalization failed"
+assert((marginal_test_2 == np.array([3,3])).all()), "Marginalization failed"
+test_dist = np.array([0.0,1.0,2.0,1.0,0.0])
+marginal_test_1 = aabb.marginalize(test_dist, 0)
+assert((marginal_test_1 == test_dist).all()), "1D marginalization failed"
+try:
+    aabb.marginalize(test_dist, 2)
+    raise AssertionError("")    
+except AssertionError:
+    raise AssertionError("Failed check that marginalization axis is reasonable")
+except:
+    pass
+
+test_dist = np.array([0,1,2,3,4,5,4,3,2,1,0,0,0,0])/25
+bound_test = aabb.marginal_bound(test_dist, 0.1)
+assert(bound_test == [2, 9]), "Marginal bounding failed"
+test_dist = np.array([0,0,0,0,0,0,0,1,1,1,1,1,1,2,1,1,0,0])/10
+bound_test = aabb.marginal_bound(test_dist, 0.25)
+assert(bound_test == [9, 16]), "Marginal bounding failed with nearly flat distribution"
+test_dist = np.array([0,0,0,0,0,0,0,1,1,1,1,1,1,2,1,1,0,0])
+bound_test = aabb.marginal_bound(test_dist, 0.25)
+assert(bound_test == [9, 16]), "Marginal bounding failed with unnormalized distribution"
+bound_test = aabb.marginal_bound(test_dist, 0.0)
+assert(bound_test == [7, 16]), "Marginal bounding failed with eps=0.0"
+bound_test = aabb.marginal_bound(test_dist, 1.0)
+assert(bound_test == []), "Marginal bounding failed with eps=1.0"
+
+test_dist = np.array([[1,2,3,4],[5,6,7,8]])
+trunc_test = aabb.trunc_dist(test_dist, [[0,1],[0,3]])
+assert((trunc_test == [[1,2,3]]).all()), "dist truncation failed"
+try:
+    trunc_test = aabb.trunc_dist(test_dist, [[0,3],[0,1]])
+    raise AssertionError("")
+except AssertionError:
+    raise AssertionError("Failed check that bounds are right size")
+except:
+    pass
+try:
+    trunc_test = aabb.trunc_dist(test_dist, [[0,1,1],[0,3]])
+    raise AssertionError("")
+except AssertionError:
+    raise AssertionError("Failed check that bounds are right length")
+except:
+    pass
+try:
+    trunc_test = aabb.trunc_dist(test_dist, [[2,0],[0,3]])
+    raise AssertionError("")
+except AssertionError:
+    raise AssertionError("Failed check that bounds are right order")
+except:
+    pass
+try:
+    trunc_test = aabb.trunc_dist(test_dist, [[0,1]])
+    raise AssertionError("")
+except AssertionError:
+    raise AssertionError("Failed check that bounds and dist have same dimensionality")
+except:
+    pass
+print("All unit tests passed!")
+print("")
+
+print("Functional testing...")
 print("Test case 1")
 epsilon = 0.2
 test_input = 2.0*np.array([0.0,0.0,0.01,0.02,0.04,0.08,0.16,0.38,0.16,0.08,0.04,0.02,0.01,0.0,0.0,0.0,0.0])
-my_aabb = draw_aabb(test_input, epsilon, verbose=True)
+my_aabb = aabb.draw_aabb(test_input, epsilon, verbose=True)
 #print([my_aabb[key] for key in ["bounds","norm","distribution"]])
 assert(my_aabb["bounds"] == [[5, 10]]), "Failed test 1"
 print("...ok")
-print("")
 
 print("Test case 2")
 epsilon = 0.2
 test_input = np.array([[0.0,0.0,0.02,0.0,0.0],[0.0,0.04,0.08,0.04,0.0],[0.04,0.06,0.10,0.08,0.04],[0.04,0.06,0.10,0.08,0.04],[0.0,0.04,0.08,0.04,0.0],[0.0,0.0,0.02,0.0,0.0]])
-my_aabb = draw_aabb(test_input, epsilon, verbose=True)
+my_aabb = aabb.draw_aabb(test_input, epsilon, verbose=True)
 #print([my_aabb[key] for key in ["bounds","norm","distribution"]])
 assert(my_aabb["bounds"] == [[1, 5],[1, 5]]), "Failed test 2"
 print("...ok")
-print("")
 
 print("Test case 3")
 epsilon = 0.2
 test_input = np.array([0.0,0.0,0.0,0.0,0.0,0.0,0.9,0.1,0.0])
-my_aabb = draw_aabb(test_input, epsilon, verbose=True)
+my_aabb = aabb.draw_aabb(test_input, epsilon, verbose=True)
 #print([my_aabb[key] for key in ["bounds","norm","distribution"]])
 assert(my_aabb["bounds"] == [[6, 7]]), "Failed test 3"
 print("...ok")
-print("")
 
 print("Test case 4")
 epsilon = 0.02
@@ -43,11 +106,10 @@ x, y = np.mgrid[minpos:maxpos:spacing, minpos:maxpos:spacing]
 volume_element = spacing**2
 pos = np.stack((x, y), axis=2)
 test_input = np.array(volume_element*rv.pdf(pos))
-my_aabb = draw_aabb(test_input, epsilon)
+my_aabb = aabb.draw_aabb(test_input, epsilon)
 #print(my_aabb)
 assert(my_aabb == [[87, 113],[87, 113]]), "Failed test 4"
 print("...ok")
-print("")
 
 print("Test case 5")
 epsilon = 0.01
@@ -59,79 +121,71 @@ x1, x2, x3, x4, x5 = np.mgrid[minpos:maxpos:spacing, minpos:maxpos:spacing, minp
 volume_element = spacing**5
 pos = np.stack((x1, x2, x3, x4, x5), axis=5)
 test_input = np.array(volume_element*rv.pdf(pos))
-my_aabb = draw_aabb(test_input, epsilon)
+my_aabb = aabb.draw_aabb(test_input, epsilon)
 #print(my_aabb)
 assert(my_aabb == [[8, 15], [9, 16], [8, 15], [7, 14], [8, 15]]), "Failed test 5"
 print("...ok")
-print("")
 
 print("Test case 6")
 epsilon = 0.21
 test_input = np.array([0,0,0,0,0,0,0,0,0,1,1,1,1,1,2,1,1,1,0,0])/10
-my_aabb = draw_aabb(test_input, epsilon, verbose=True)
+my_aabb = aabb.draw_aabb(test_input, epsilon, verbose=True)
 #print([my_aabb[key] for key in ["bounds","norm","distribution"]])
 assert(my_aabb["bounds"] == [[11, 18]]), "Failed test 6"
 print("...ok")
-print("")
 
 print("Test case 7")
 epsilon = 0.1
 test_input = np.array([1.0])
-my_aabb = draw_aabb(test_input, epsilon, verbose=True)
+my_aabb = aabb.draw_aabb(test_input, epsilon, verbose=True)
 #print([my_aabb[key] for key in ["bounds","norm","distribution"]])
 assert(my_aabb["bounds"] == [[0, 1]]), "Failed test 7"
 print("...ok")
-print("")
 
 print("Test case 8")
 epsilon = 0.1
 test_input = np.array([0.0])
 try:
-    my_aabb = draw_aabb(test_input, epsilon)
+    my_aabb = aabb.draw_aabb(test_input, epsilon)
     raise AssertionError("")
 except AssertionError:
     raise AssertionError("Failed test 8")
 except:
     print("...ok")
-    print("")
 
 print("Test case 9")
 epsilon = 0.1
 test_input = np.array([0.0,0.0,0.1,-0.1,0.01,0.02,0.04,0.08,0.16,0.38,0.16,0.08,0.04,0.02,0.01,0.0,0.0,0.0,0.0])
 try:
-    my_aabb = draw_aabb(test_input, epsilon)
+    my_aabb = aabb.draw_aabb(test_input, epsilon)
     raise AssertionError("")
 except AssertionError:
     raise AssertionError("Failed test 9")
 except:
     print("...ok")
-    print("")
 
 print("Test case 10")
 epsilon = 0.1
 test_input = np.array([])
 try:
-    my_aabb = draw_aabb(test_input, epsilon)
+    my_aabb = aabb.draw_aabb(test_input, epsilon)
     raise AssertionError("")
 except AssertionError:
     raise AssertionError("Failed test 10")
 except:
     print("...ok")
-    print("")
 
 print("Test case 11")
 epsilon = 1.0
 test_input = np.array([0.0,0.2,0.3,0.3,0.2,0.0])
-my_aabb = draw_aabb(test_input, epsilon)
+my_aabb = aabb.draw_aabb(test_input, epsilon)
 assert(my_aabb == [[]]), "Failed test 11"
 print("...ok")
-print("")
 
 print("Test case 12")
 epsilon = 0.0
 test_input = np.array([0.0,0.2,0.3,0.3,0.2,0.0])
-my_aabb = draw_aabb(test_input, epsilon)
+my_aabb = aabb.draw_aabb(test_input, epsilon)
 assert(my_aabb == [[1, 5]]), "Failed test 12"
 print("...ok")
-print("")
-
+print("All functional tests passed!")
